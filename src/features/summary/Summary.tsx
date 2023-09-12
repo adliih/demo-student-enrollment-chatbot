265c8c46082f4537b38e0aafc89d34924ce4200e
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-09-12 14:43:21 +0700

    add link to home page in summary

--- src/features/summary/Summary.tsx
+++ src/features/summary/Summary.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { useAppSelector } from "../../app/hooks"
+import { Link } from "react-router-dom"
 
 export default function Summary() {
   const { slot, age, name } = useAppSelector((state) => state.studentInfo)
@@ -7,7 +8,7 @@ export default function Summary() {
   return (
     <div>
       Your name {name} aged {age} has been added to student system at {date}{" "}
-      {time}. You may now exit.
+      {time}. You may now <Link to="/">exit</Link>.
     </div>
   )
 }
