30b2c0209280aa1e566f7e958e994d80b38e0fe9
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-09-12 16:53:33 +0700

    fix UT

--- src/features/student-info/studentInfoSlice.spec.ts
+++ src/features/student-info/studentInfoSlice.spec.ts
@@ -5,23 +5,27 @@ import studentInfoReducer, {
 } from "./studentInfoSlice"
 
 describe("studentInfoSlice", () => {
-  const initialState: StudentInfoState = {}
+  const initialState: StudentInfoState = { slot: {} }
 
   it("should store submitted name", () => {
     const name = "submitted name"
     expect(
       studentInfoReducer(initialState, nameSubmitted(name)),
-    ).toEqual<StudentInfoState>({
-      name,
-    })
+    ).toEqual<StudentInfoState>(
+      expect.objectContaining({
+        name,
+      } as Partial<StudentInfoState> as StudentInfoState),
+    )
   })
 
   it("should store submitted age", () => {
     const age = 18
     expect(
       studentInfoReducer(initialState, ageSubmitted(age)),
-    ).toEqual<StudentInfoState>({
-      age,
-    })
+    ).toEqual<StudentInfoState>(
+      expect.objectContaining({
+        age,
+      } as Partial<StudentInfoState> as StudentInfoState),
+    )
   })
 })
